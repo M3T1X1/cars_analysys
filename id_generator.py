7177f53b062bc332c342cad4043998fee3ebc2dc
M3T1X1 <dusza-kacper@wp.pl>
2024-08-04 19:02:00 +0200

    I made that scripts automaticly write the input values of vin and id into the excel worksheets

--- id_generator.py
+++ id_generator.py
@@ -1,5 +1,14 @@
-import string
 import random
+from openpyxl import load_workbook
 
 id = ''.join(random.choice('QWERTYUIOPASDFGHJKLZXCVBNM1234567890') for i in range(11))
-print('ID:',id)
+file = 'Cars.xlsx'
+data = [[id]]
+
+wb = load_workbook(file)
+ws = wb["Sellers"]
+
+for seller_id in data:
+    ws.append(seller_id)
+
+wb.save(file)
--- vin_generator.py
+++ vin_generator.py
@@ -1,5 +1,14 @@
-import string
 import random
+from openpyxl import load_workbook
 
 vin = ''.join(random.choice('WERTYUIOPASDFGHJKLZXCVBNM1234567890') for i in range(17))
-print('VIN: ',vin)
+file = 'Cars.xlsx'
+data = [[vin]]
+
+wb = load_workbook(file)
+ws = wb.active
+
+for vin in data:
+    ws.append(vin)
+
+wb.save(file)
